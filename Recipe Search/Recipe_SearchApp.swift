29c8040a738ef46d1c7f92a994a098fe444b4f17
Kelvin Ricafort <kelvs.r@icloud.com>
2024-06-21 22:22:10 +0800

    Initial Commit

---
+++ Recipe Search/Recipe_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_SearchApp.swift
+//  Recipe Search
+//
+//  Created by Kelvin on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
