2cb081a7ef96f7d4efe233972b88cdf403823aa5
Kelvin Ricafort <kelvs.r@icloud.com>
2024-06-22 17:36:47 +0800

    - Added UI Elementd

--- Recipe Search/ContentView.swift
+++ Recipe Search/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var recipes = [Recipe]()
+    @State private var searchTerm = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                TextField("Search for recipes", text: $searchTerm)
+                    .textFieldStyle(.roundedBorder)
+                Button("Search") {
+                    searchForRecipes()
+                }
+            }
+            List(recipes) { recipe in
+                HStack(spacing: 10) {
+                    AsyncImage(url: URL(string: recipe.image)) { image in
+                        image
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .aspectRatio(contentMode: .fill)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Text(recipe.title)
+                }
+            }
+            .listStyle(.plain)
+            .scrollIndicators(.hidden)
         }
         .padding()
+    }
+    
+    func searchForRecipes() {
+        Task {
+            self.recipes = await DataService().getRecipes(term: searchTerm)
+        }
     }
 }
 
