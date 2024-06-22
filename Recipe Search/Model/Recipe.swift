b1f40b377840569d53b3936a179f4c6449772450
Kelvin Ricafort <kelvs.r@icloud.com>
2024-06-22 16:48:35 +0800

    - Add Models

---
+++ Recipe Search/Model/Recipe.swift
@@ -0,0 +1,8 @@
+//
+//  Recipe.swift
+//  Recipe Search
+//
+//  Created by Kelvin on 6/22/24.
+//
+
+import Foundation
---
+++ Recipe Search/Model/RecipeResponse.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeResponse.swift
+//  Recipe Search
+//
+//  Created by Kelvin on 6/22/24.
+//
+
+import Foundation
